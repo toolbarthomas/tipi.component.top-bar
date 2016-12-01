1d618b8ad595d899dfb4a802876615e0a74e4a37
Thomas van der Velde <contact@toolbarthomas.net>
2016-12-01 14:15:59 +0100

    Task: Add class on html

--- tipi.component.top-bar.js
+++ tipi.component.top-bar.js
@@ -15,6 +15,8 @@ function setTopBar(pushElement, smallElement) {
 
 	var topBar = $('.top-bar').not('.' + topBarStates.ready);
 	if(topBar.length > 0 ) {
+		var html = $('html');
+
 		//Define the push location for the topBar so it won't overlap other elements by default.
 		topBar.after('<div class="top-bar-push-fallback"></div>');
 		var topBarPushElement = $('.top-bar-push-fallback'); //By default we use the fallback so we always have a pushing element.
@@ -63,7 +65,7 @@ function setTopBar(pushElement, smallElement) {
 		});
 
 		topBar.addClass(topBarStates.ready);
-		$('html').addClass(topBarStates.ready);
+		html.addClass(topBarStates.ready);
 		topBar.data(topBarDataAttributes.position, 0);
 
 		topBar.trigger('tipi.topBar.RESIZE', [0]);
@@ -72,6 +74,8 @@ function setTopBar(pushElement, smallElement) {
 }
 
 function toggleTopBar(topBar, topBarSmallElement, topBarStates, topBarDataAttributes) {
+	var html = $('html');
+
 	var triggers = {
 		scrollTop : Math.ceil($(window).scrollTop()),
 		smallPosition : Math.ceil(topBarSmallElement.position().top),
@@ -91,24 +95,30 @@ function toggleTopBar(topBar, topBarSmallElement, topBarStates, topBarDataAttrib
 	//When the scrollTop of the Window is higher than the position + height of the top bar then we can we make it smaller.
 	if(triggers.scrollTop >= (triggers.smallPosition + triggers.smallHeight)) {
 		topBar.addClass(topBarStates.small);
+		html.addClass(topBarStates.small);
 	} else {
 		topBar.removeClass(topBarStates.small);
+		html.removeClass(topBarStates.small);
 	}
 
 	//When the scrollTop of the Window is beyond the smallElement ofsset times 2
 	if(triggers.scrollTop >= ((triggers.smallPosition + triggers.smallHeight) * 2)) {
 		if(!topBar.hasClass(topBarStates.upwards)) {
 			topBar.addClass(topBarStates.hidden);
+			html.addClass(topBarStates.hidden);
 		}
 	} else {
 		topBar.removeClass(topBarStates.hidden);
+		html.removeClass(topBarStates.hidden);
 	}
 
 	//Set the peek state on the top-bar when the user scrolls upwards
 	if(triggers.scrollTop < topBarPositionCache) {
 		topBar.addClass(topBarStates.peek + ' ' + topBarStates.upwards);
+		html.addClass(topBarStates.peek + ' ' + topBarStates.upwards);
 	} else {
 		topBar.removeClass(topBarStates.peek + ' ' + topBarStates.upwards);
+		html.removeClass(topBarStates.peek + ' ' + topBarStates.upwards);
 	}
 
 	//Reset the top bar when reached the top of the Document
@@ -119,9 +129,15 @@ function toggleTopBar(topBar, topBarSmallElement, topBarStates, topBarDataAttrib
 }
 
 function resizeTopBarPush(topBar, topBarPushElement, topBarSmallElement, topBarStates, topBarDataAttributes, resizeAnimationSpeed) {
+	var html = $('html');
+
 	//Reset the height without transitions so we can calculate the original height.
 	topBar.addClass(topBarStates.reset);
 	topBar.removeClass(topBarStates.small);
+
+	html.addClass(topBarStates.reset);
+	html.removeClass(topBarStates.small);
+
 
 	var topBarHeight = topBar.outerHeight();
 	topBar.data(topBarDataAttributes.originalHeight, topBarHeight);
@@ -134,5 +150,6 @@ function resizeTopBarPush(topBar, topBarPushElement, topBarSmallElement, topBarS
 		'height' : topBarHeight
 	}, resizeAnimationSpeed, function() {
 		topBar.removeClass(topBarStates.reset);
+		html.removeClass(topBarStates.reset);
 	});
 }
